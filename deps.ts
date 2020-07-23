d9eecf9bdee643d32a7a0cb899c0afd8c7407f6c
S <sushil@appsynth.net>
2020-07-24 03:41:32 +0700

    Specific version uses

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 //Standard
-export * as log from "https://deno.land/std/log/mod.ts";
+export * as log from "https://deno.land/std@0.61.0/log/mod.ts";
 
 //3rd party
 export { nanoid } from "https://deno.land/x/nanoid/mod.ts";
-export { Router, Application, send } from "https://deno.land/x/oak/mod.ts";
+export { Router, Application, send } from "https://deno.land/x/oak@v6.0.1/mod.ts";
